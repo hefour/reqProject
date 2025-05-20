8fff95c5d097cc487e783f7db161c42dc878ed59
chounjae <blueskycuj@naver.com>
2025-05-20 23:17:19 +0900

    test 벡터 초안/MatrixByArr 의 2차원 배열 int -> Scalar변경

--- src/tensor/Tensors.java
+++ src/tensor/Tensors.java
@@ -13,8 +13,18 @@ public class Tensors {
         System.out.println("현재 생성된 스칼라는 "+scalar.toString()+"입니다. ");
 
     }
-    public static void validateVector(Vector vector) {
+    public static void validateVector(Vector vector,String input) {
+        Vector expectedVector = new VectorImpl(vector.size(), input);
+        System.out.println("연산 결과 값은 "+ vector+"입니다.");
+        if(expectedVector.equals(vector))
+            System.out.println("비교 결과 true!");
+        else
+            System.out.println("비교 결과 false...");
 
+    }
+
+    public static void printVector(Vector vector) {
+        System.out.println("현재 생성된 백터는 " +vector.toString() + "입니다. ");
     }
     public static void validateMatrix(Matrix matrix) {
 
